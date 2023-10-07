348f947a06cb7114812542785afc73e54989c32c
benja27 <50673336+benja27@users.noreply.github.com>
2023-10-07 15:45:01 -0600

    adding styles

--- src/components/index_item.jsx
+++ src/components/index_item.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 function Index_item() {
   return (
-    <div>
-
+    <div style={{width:"200px"}} >
+      
+      <div>
+        <img className='index_item_img' src="https://previews.123rf.com/images/pandavector/pandavector1705/pandavector170502139/77686847-doctor-profesiones-%C3%BAnico-icono-en-estilo-de-dibujos-animados-s%C3%ADmbolo-de-vectores-stock-photography.jpg" alt="" />
+      </div>
 
       <h5>VESPA C20</h5>
+      <h4 style={{color: 'gray' }}  >.......................</h4>
       <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quia quae rerum tempore odit laborum </h6>
+      <div className='index_item_social_cont d-flex justify-content-center gap-3 text-center' >
+        <i className='fab fa-facebook' ></i>
+        <i className='fab fa-twitter' ></i>
+        <i className='fas fa-home' ></i>
+      </div>
 
-      
+
     </div>
   )
 }
