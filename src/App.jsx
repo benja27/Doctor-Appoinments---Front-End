f54aeae6b33757bd13b4e666d11c7a2ff93d7c1e
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-11 00:10:19 +0300

    Add user authentication links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import User from './components/authentication/User';
+//import PrivateText from './components/authentication/PrivateText';
 import { Route, Routes } from 'react-router-dom';
 import DoctorsContainer from './components/doctors/DoctorsContainer';
 import DoctorForm from './components/doctors/DoctorForm';
 import Header from './components/doctors/Header';
 import ShowDoctor from './components/doctors/ShowDoctor';
 import DeleteDoctorContainer from './components/doctors/DeleteDoctorContainer';
+
 // import Main_page from './components/main_page'
 // import Set_appoiment from './components/set_appoiment'
 // import Show from './components/show'
@@ -11,6 +15,7 @@ import DeleteDoctorContainer from './components/doctors/DeleteDoctorContainer';
 import './App.css';
 
 function App() {
+  const [currUser, setCurrUser] = useState(null);
   return (
   // <Routes>
   //   <Route path="/" element={<Main_page />} />
@@ -19,6 +24,7 @@ function App() {
   //   <Route path="/set_appoinment" element={<Set_appoiment  />} />
   // </Routes>
     <>
+      <User currUser={currUser} setCurrUser={setCurrUser} />
       <Header />
       <Routes>
         <Route path="/" element={<DoctorsContainer />} />
