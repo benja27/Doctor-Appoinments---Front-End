52a9f54a60439dee84aa257e05c1b6d77727d9aa
ClintonYounge <clintonyounge18@gmail.com>
2023-10-10 12:28:35 -0400

    Run linters

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
