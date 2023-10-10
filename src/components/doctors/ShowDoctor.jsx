cf0857215dcdee269cf4b62114033fcdbc8de492
ClintonYounge <clintonyounge18@gmail.com>
2023-10-10 03:44:40 -0400

    Add a show doctor component
    - When the showDoctor slice function is deployed it adds the doctor to show to the state. Then renders the doctor from the state

---
+++ src/components/doctors/ShowDoctor.jsx
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchDoctors, deleteDoctor, showDoctor } from '../../redux/doctors/doctorsSlice';
+
+export default function ShowDoctor() {
+  const { doctorId } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(showDoctor(doctorId));
+  }, [dispatch, doctorId]);
+
+  const doctor = useSelector((state) => state.doctors.showDoctor);
+  const isLoading = useSelector((state) => state.doctors.isLoading);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  const handleDeleteDoctor = async () => {
+    try {
+      await dispatch(deleteDoctor(doctor.id));
+      
+      dispatch(fetchDoctors());
+    } catch (error) {
+      throw new Error('Failed to delete doctor');
+    }
+  };
+
+  return (
+    <>
+        {doctor && (
+          <ul>
+            <li>Name: {doctor.name}</li>
+            <li>Specialization: {doctor.specialization}</li>
+            <li>Consultation Fee: {doctor.consultation_fee}</li>
+            <li>Prescription Fee: {doctor.prescription_fee}</li>
+            <button
+              type="button"
+              onClick={handleDeleteDoctor}
+            >
+              Delete Doctor
+            </button>
+          </ul>
+        )}
+    </>
+  );
+}
