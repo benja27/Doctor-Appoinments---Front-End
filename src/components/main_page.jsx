4642f21bb7f3b467bfbede1fa8b041feb4f74bcd
emarkees <rphillipse@gmail.com>
2023-10-11 17:33:42 -0600

    Update main_page files

--- src/components/main_page.jsx
+++ src/components/main_page.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import SideMenu from './side_menu';
-import Corrousel from './corrousel';
-import data from './doctor';
+import Side_menu from './side_menu'; // Replace with the correct import path
+import Corrousel from './corrousel'; // Import the correct component
+import data from './doctor'; // Import the data source
 
 function Main_page() {
   const [searchItem, setSearchItem] = useState('');
@@ -19,57 +19,54 @@ function Main_page() {
     item.name.toLowerCase().includes(searchItem.toLowerCase())
   );
 
-  const transit = filteredData.map((doc, index) => (
-    <div className='doc' key={index}>
-      <span className=''>{doc.name}</span>
-      <span className=''>{doc.specialization}</span>
-    </div>
-  ));
-
   return (
-    <div className='d-flex flex-column nav-bg'>
-      {isMenuOpen && (
-        <div className='p-0 m-0 w-0 h-0 bg-light'>
-          <a className="icon m-3 mt-3 p-0" onClick={toggleMenu}>
-            <i className="fa fa-times fa-2x mt-3 m-0 p-0 mb-0"></i>
-          </a>
-          <SideMenu isMenuOpen={isMenuOpen} />
+    <div className="grid d-flex justify-content-evenly flex-column nav-bg">
+      <div className="row">
+        <div className="col-md-3">
+          <Side_menu isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
         </div>
-      )}
-      <nav className='d-flex justify-content-between align-items-center mr-3 p-2 m-0 w-auto'>
-        <a className="icon bg-black gap-6 m-2" onClick={toggleMenu}>
-          <i className="fa fa-bars fa-2x mt-2"></i>
-        </a>
-        <section className='search mr-3 d-none d-md-block'>
-          <input
-            type='text'
-            placeholder='Search'
-            className='search rounded-pill none'
-            value={searchItem}
-            onChange={handleSearchInputChange}
-          />
-        </section>
-      </nav>
+        <div className="col-md-9 mt-3">
+          <nav className="navbar d-flex justify-content-between align-items-center m-0 p-2">
+            <section className="search mr-3" style={{ display: 'none' }}>
+              <input
+                type="text"
+                placeholder="Search"
+                className="search rounded-pill w-100"
+                value={searchItem}
+                onChange={handleSearchInputChange}
+              />
+            </section>
+          </nav>
+          <main className=" main-body d-flex row text-light justify-contents-between bg-black mt-6 h-100">
+            <div className="greetings text-center">
+              <h1 className="display-3 fw-bold">Your health is our concern!</h1>
+              <p className="text-x-large fw-semi-bold">How are you feeling today</p>
+            </div>
 
-      <main className='d-flex row text-light justify-content-evenly'>
-        <div className='greetings d-flex flex-column justify-content-center align-items-center mt-5'>
-          <h1 className='display-3 fw-bold text-center'>Your health is our concern!</h1>
-          <p className='tetx-x-large fw-semi-bold text-center'>How are you feeling today</p>
+            <div className="start_cont none justify-content-center align-items-center mt-5">
+              <div className="d-none container_starter flex-column align-items-center">
+                <button className="start_container rounded-pill w-75 p-3">Start medication here</button>
+              </div>
+            </div>
+
+            <div className="login-container d-flex flex-row justify-content-center align-items-center mt-6">
+              <button className="btn sign-up-btn mb-3" type="button">
+                Signup
+              </button>
+              <button
+                className="btn login-up-btn"
+                type="button"
+                onClick={() => {
+                  console.log('Login button clicked');
+                }}
+              >
+                Login
+              </button>
+            </div>
+          </main>
+          <Corrousel />
         </div>
-        <div className='start_cont w-md-25 d-flex justify-content-center align-items-center mt-5 lg-none'>
-          <div className='container_starter d-flex col w-lg-25 justify-content-evenly align-items-center'>
-            <button className=' start_container rounded-pill w-75 p-3'>Start medication here</button>
-          </div>
-        </div>
-        <div className='d-flex row login-container align-items-center justify-content-evenly'>
-          <button className='btn sign-up-btn' type='button'>Signup</button>
-          <button className='btn login-up-btn' type='button' onClick={() => {
-              console.log('Login button clicked');
-              // Add your login logic here if needed
-            }}>Login</button>
-        </div>
-      </main>
-      <Corrousel />
+      </div>
     </div>
   );
 }
--- src/components/side_menu.jsx
+++ src/components/side_menu.jsx
@@ -1,72 +1,50 @@
 import React, { useState } from 'react';
-import doc from '../assets/image/doc1.jpg';
 
-function SideMenu({ isMenuOpen }) {
-  const [clickedItem, setClickedItem] = useState(null);
+function Side_menu({ isMenuOpen, toggleMenu }) {
+  const [isContentVisible, setIsContentVisible] = useState(false);
 
-  const handleItemClick = (index) => {
-    setClickedItem(index === clickedItem ? null : index);
+  const toggleContentVisibility = () => {
+    setIsContentVisible(!isContentVisible);
   };
 
-  const sideItems = [
-    { text: 'Model', link: 'http://example.com' },
-    { text: 'Specialization', link: 'http://example.com' },
-    { text: 'Model', link: 'http://example.com' },
-  ];
-
   return (
-    <div className={`d-flex nav col justify-content-center align-items-center ${isMenuOpen ? 'open' : ''}`}>
-      <div>
-        <img src={doc} alt="" className='logo mt-5' />
+    <main className={`na d-flex flex-column justify-content-between gap-4 ps-3 mb-4 px-3 col-md-3 col-lg-2 ${isMenuOpen ? 'open' : ''}`}>
+      <a onClick={() => { toggleMenu(); toggleContentVisibility(); }} className={`icon bg-black d-md-none  ${isMenuOpen ? 'text-white'  : ''}`}>
+        <i className="fa fa-bars fa-2x mt-2"></i>
+      </a>
+      <div className={`side-menu d-none ${isContentVisible ? 'show' : ''}`} style={{ height: '100vh', overflow: 'hidden' }}>
+        {isContentVisible && 
+           <div className="row bg-light">
+           <div className="text-center">
+             <img src="https://placehold.co/150x50?text=Brand" alt="" />
+           </div>
+           <div className="mt-1 side_menu_cont pt-5 d-flex flex-column gap-4 text-center">
+             <section>
+               <a href="/">
+                 <h4 className="h4 fw-bold">Home</h4>
+               </a>
+             </section>
+             <section>
+               <a href="/add-doctor">
+                 <h4 className="h4 fw-bold">Add Doctor</h4>
+               </a>
+             </section>
+             <section>
+               <a href="/delete-doctor">
+                 <h4 className="h4 fw-bold">Delete Doctor</h4>
+               </a>
+             </section>
+             <section>
+               <a href="/my-appointments">
+                 <h4 className="h4 fw-bold">My Appointments</h4>
+               </a>
+             </section>
+           </div>
+         </div>
+        }
       </div>
-      <nav className='d-flex justify-content-center p-3'>
-        <div className={`topnav d-flex flex-column align-items-center vh-100 w-lg-75 ${isMenuOpen ? 'show' : ''}`}>
-          {isMenuOpen && (
-            <ul className='menu justify-content-center mt-3 gap-6 m-0 p-0'>
-              {sideItems.map((item, index) => (
-                <li
-                  key={index}
-                  className={`side-item mt-3 m-4 text-center ${index === clickedItem ? 'active' : ''}`}
-                  onClick={() => handleItemClick(index)}
-                >
-                  <a href={item.link} className='text-decoration-none'>
-                    <h4 className='m-3 px-3'>{item.text}</h4>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          )}
-          {isMenuOpen && (
-            <div className='d-flex row justify-content-center align-items-center mt-5 m-0 no-wrap'>
-              <ul className='d-flex col justify-content-center align-content-center list-unstyled gap-3 m-0 p-0'>
-                <li className='p-1'>
-                  <i className='fa fa-twitter'>
-                    <a href="http://twitter.com"></a>
-                  </i>
-                </li>
-                <li className='p-1'>
-                  <i className='fa fa-facebook'>
-                    <a href="http://facebook.com.com"></a>
-                  </i>
-                </li>
-                <li className='p-1'>
-                  <i className='fa fa-linkedin'>
-                    <a href="http://linkedin.com"></a>
-                  </i>
-                </li>
-                <li className='p-1'>
-                  <i className='fa fa-pinterest-square'>
-                    <a href="http://pinterest.com"></a>
-                  </i>
-                </li>
-              </ul>
-              <p className='m-0 mt-2 text-center fs-5'>We care, but God Heals</p>
-            </div>
-          )}
-        </div>
-      </nav>
-    </div>
+    </main>
   );
 }
 
-export default SideMenu;
+export default Side_menu;
