255d9fdd694d9fbf86b9a7e1fcf22e9e5e8577fc
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-12 01:10:57 +0300

    Store token in the state

--- src/redux/currentUser/currentUserSlice.jsx
+++ src/redux/currentUser/currentUserSlice.jsx
@@ -2,87 +2,131 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
     currentUser: {},
-    token: JSON.parse(localStorage.getItem("token")) || "",
-    isLoading: false,
-    error: undefined,
+    token: "",
+    isAuth: false,
+    error: null
     };
 
-export const signupUser = createAsyncThunk('currentUser/signupUser', async (user) => {
-    try {
-        const response = await fetch('http://localhost:3001/signup', {
+export const signupUser = createAsyncThunk('currentUser/signup', async (userInfo) => {
+    try{
+        const url = "http://localhost:3001/signup"
+        const res = await fetch(url, {
             method: "POST",
-            body: JSON.stringify(user),
+            body: JSON.stringify(userInfo),
             headers: {
-                "content-type": "application/json",
+                "Content-Type": "application/json"
             }
-        });
-        localStorage.setItem("token", res.headers.get("Authorization"))
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        throw new Error('Failed to signup user');
+        })
+        const data = await res.json();
+        if(res.ok){
+            const token = res.headers.get("Authorization")
+            const user = data.data.data
+            localStorage.setItem('token', token);
+            localStorage.setItem('user', JSON.stringify(user));
+            return { token, user };
+        }
+        throw data.error    
+    }
+    catch(error){
+        console.log(error)
+        return error
     }
 }
-);
 
-export const loginUser = createAsyncThunk('currentUser/loginUser', async (user) => {
-    try {
-        const response = await fetch('http://localhost:3001/login', {
+
+)
+
+export const loginUser = createAsyncThunk('currentUser/login', async (userInfo) => {
+    try{
+        const url = "http://localhost:3001/login"
+        const res = await fetch(url, {
             method: "POST",
-            body: JSON.stringify(user),
+            body: JSON.stringify(userInfo),
             headers: {
-                "content-type": "application/json",
+                "Content-Type": "application/json"
             }
-        });
-        localStorage.setItem("token", response.headers.get("Authorization"))
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        throw new Error('Failed to login user');
+        })
+        const data = await res.json();
+        if(res.ok){
+            const token = res.headers.get("Authorization")
+            const user = data.data.data
+            localStorage.setItem('token', token);
+            localStorage.setItem('user', JSON.stringify(user));
+            return { token, user };
+        }
+        throw data.error    
+    }
+
+    catch(error){
+        console.log(error)
+        return error
+    }
+
+}
+)
+
+export const logoutUser = createAsyncThunk('currentUser/logout', async ({dispatch}) => {
+    try{
+        const url = "http://localhost:3001/logout"
+        const res = await fetch(url, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token")
+            },
+        })
+        const data = await res.json();
+        if(res.ok){
+            dispatch(logout())
+            localStorage.removeItem("token")
+            localStorage.removeItem("user")
+            return data;
+        }
+        throw data.error    
+    }
+    catch(error){
+        console.log(error)
+        return error
     }
 }
-);
+)
+
+
+
 
 export const currentUserSlice = createSlice({
     name: 'currentUser',
     initialState,
-    reducers: {
-        logoutUser: (state) => {
-            state.currentUser = {};
-            state.token = "";
-        }
-    },
-    extraReducers(builder) {
-        builder.addCase(signupUser.pending, (state, action) => {
-            state.isLoading = true;
-        });
-        builder.addCase(signupUser.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.currentUser = action.payload;
-        });
-        builder.addCase(signupUser.rejected, (state, action) => {
-            state.isLoading = false;
-            state.error = action.error.message;
-        });
-        builder.addCase(loginUser.pending, (state, action) => {
-            state.isLoading = true;
-        });
-        builder.addCase(loginUser.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.currentUser = action.payload;
-        });
-        builder.addCase(loginUser.rejected, (state, action) => {
-            state.isLoading = false;
-            state.error = action.error.message;
-        });
-    }
-    
-});
+    reducers:{
+        authRequest: (state) =>( {
+            ...state,
+            loading: true            
+        }),
+        authSuccess: (state, action) => ({
+            ...state,
+            currentUser: action.payload.user,
+            token: action.payload.token,
+            isAuth: true
+        }),
+        authFailure: (state, action) => ({
+            ...state,
+            isAuth: false,
+            error: action.payload
+        }),
+        logout: (state) => ({
+            ...state,
+            currentUser: {},
+            token: "",
+            isAuth: false
+        })
 
-export const { logoutUser } = currentUserSlice.actions;
+    }
+})
+
+export const { authRequest, authSuccess, authFailure, logout } = currentUserSlice.actions;
+
+export const selectCurrentUser = (state) => state.currentUser.currentUser;
+export const selectToken = (state) => state.currentUser.token;
 
 export default currentUserSlice.reducer;
-
-
-
 
