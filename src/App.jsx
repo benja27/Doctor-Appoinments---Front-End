2ba4138301028fc87c1becd6d2f938352699eedb
benja27 <50673336+benja27@users.noreply.github.com>
2023-10-10 21:28:35 -0600

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import DoctorForm from './components/doctors/DoctorForm';
 import Header from './components/doctors/Header';
 import ShowDoctor from './components/doctors/ShowDoctor';
 import DeleteDoctorContainer from './components/doctors/DeleteDoctorContainer';
-// import Main_page from './components/main_page'
+import Main_page from './components/main_page'
 // import Set_appoiment from './components/set_appoiment'
 // import Show from './components/show'
 // import './App.css'
@@ -21,7 +21,8 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<DoctorsContainer />} />
+        {/* <Route path="/" element={<DoctorsContainer />} /> */}
+        <Route path="/" element={<Main_page />} />
         <Route path="/add-doctor" element={<DoctorForm />} />
         <Route path="/doctors/:doctorId" element={<ShowDoctor />} />
         <Route path="/delete-doctor" element={<DeleteDoctorContainer />} />
--- src/components/corrousel.jsx
+++ src/components/corrousel.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
 import '../css/Carousel.css';
+import { fetchDoctors } from '../redux/doctors/doctorsSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import Doctors from './doctors/Doctors';
+
 
 const images = [
   'https://picsum.photos/id/1/300/300',
@@ -28,6 +32,11 @@ const Carousel = () => {
 
   const [itemsToShow, setItemsToShow] = useState(calculateItemsToShow());
   const [startIndex, setStartIndex] = useState(0);
+  const { doctors, isLoading, error } = useSelector((state) => state.doctors);
+  const dispatch = useDispatch();
+
+
+
   //  const [current_index, setCurrent_index] = useState(0);
 
   useEffect(() => {
@@ -39,6 +48,15 @@ const Carousel = () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
+  useEffect(() => {
+    if (doctors.length === 0) {
+      dispatch(fetchDoctors());
+    }
+    console.log(doctors)
+   }, [dispatch, doctors.length]); 
+
+
 
   const totalImages = images.length;
   const endIndex = startIndex + itemsToShow - 1;
@@ -70,32 +88,41 @@ const Carousel = () => {
           &#60;
         </button>
         <div className="image-container">
-          {images.slice(startIndex, endIndex + 1).map((image, index) => (
+
+          {/* {images.slice(startIndex, endIndex + 1).map((image, index) => ( */}
+          {doctors.slice(startIndex, endIndex + 1).map((doctor, index) => (
+            
             <div
               style={{ }}
               className={` item_slider ${index === 1 ? 'center' : ''} `}
               key={index}
             >
-              <a href="#" className="item_index_cont">
+              <a href={`doctors/`+doctor.id} className="item_index_cont">
                 <div className="index_img_circle" />
                 <div className="index_img_circle_2" />
                 <img
                   className="index_item_img position-absolute"
-                  src={`${image}`}
+                  src={`${doctor.photo}`}
                   alt=""
                 />
               </a>
 
               <div className="mt-3 text-center">
                 <h5 className="h2 fw-bold">
-                  VESPA
+                  {doctor.name}
                   {index}
                 </h5>
                 <h4 className="p-0 m-0 py-1 " style={{ color: 'gray' }}>.......................</h4>
-                <h6 className="h5 fw-bold mb-4" style={{ color: 'gray' }}>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Exercitationem, quia quae
-                </h6>
+
+                <h6 className="h5 fw-bold mb-4" style={{ color: 'gray' }}> consultatio fee: {doctor.consultation_fee } </h6>
+                <h6 className="h5 fw-bold mb-4" style={{ color: 'gray' }}> id: {doctor.id} </h6>
+                <h6 className="h5 fw-bold mb-4" style={{ color: 'gray' }}> name: {doctor.name} </h6>
+                <h6 className="h5 fw-bold mb-4" style={{ color: 'gray' }}> photo: {doctor.photo} </h6>
+                <h6 className="h5 fw-bold mb-4" style={{ color: 'gray' }}> prescription fee: {doctor.prescription_fee} </h6>
+                <h6 className="h5 fw-bold mb-4" style={{ color: 'gray' }}> especialization: {doctor.specialization} </h6>
+
+
+
                 <div className="index_item_social_cont d-flex justify-content-center gap-3 text-center">
                   <i className="fab fa-facebook" />
                   <i className="fab fa-twitter" />
@@ -103,8 +130,10 @@ const Carousel = () => {
                 </div>
               </div>
             </div>
+
           ))}
         </div>
+
         <button
           onClick={goToNextSlide}
           className="arrow right-arrow col-1"
