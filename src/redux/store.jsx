4f2f9501d4f24fd580913aa0c029908db01188dd
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-11 18:43:28 +0300

    Add currentuser reducer to store

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import doctorsReducer from './doctors/doctorsSlice';
+import currentUserReducer from './currentUser/currentUserSlice';
 
 const store = configureStore({
   reducer: {
     doctors: doctorsReducer,
+    currentUser: currentUserReducer,
   },
 });
 
