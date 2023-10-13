0f9345cd47fa4cf21284353c4739a4cbe701e25c
emarkees <rphillipse@gmail.com>
2023-10-13 10:33:21 -0600

    Add linters confiruration

--- src/components/side_menu.jsx
+++ src/components/side_menu.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 function Side_menu({ isMenuOpen, toggleMenu }) {
   return (
     <main className={`main_toggle d-flex flex-column justify-content-between gap-4 p-0 mb-4 p d-md-flex side_menu col-3 col-lg-2 ${isMenuOpen ? 'open' : ''}`}>
-      <div onClick={toggleMenu} className={`toggle d-flex p-0 justify-content-start ml-6 align-items-center d-md-none ${isMenuOpen ? 'text-white' : ''}`}>
+      <div onClick={toggleMenu} className={`toggle d-flex p-0 justify-content-start ml-6 align-items-center d-md-flex ${isMenuOpen ? 'text-white' : ''}`}>
         <i className={`d-flex fa fa-${isMenuOpen ? 'times' : 'bars'} fa-2x mt-3 mr-3`}></i>
       </div>
 
       {isMenuOpen && (
         <div className="mobile-menu with-background">
-          <div onClick={toggleMenu} className={`toggle d-flex p-0 justify-content-start ml-6 align-items-center d-md-none ${isMenuOpen ? 'text-white' : ''}`}>
+          <div onClick={toggleMenu} className={`toggle d-flex p-0 justify-content-start ml-6 align-items-center d-md-flex ${isMenuOpen ? 'text-white' : ''}`}>
         <i className={`d-flex fa fa-${isMenuOpen ? 'times': ''} fa-2x mt-3 mr-3`}></i>
       </div>
           <div className="mobile-menu-content text-center mt-5">
