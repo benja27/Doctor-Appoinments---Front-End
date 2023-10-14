2f3eeb10ee1535f99fd934f539a99d1f86e7cd47
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-14 11:15:31 +0300

    Update delete doctor

--- src/components/doctors/DeleteDoctorContainer.jsx
+++ src/components/doctors/DeleteDoctorContainer.jsx
@@ -1,18 +1,16 @@
-import { useEffect } from 'react';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteDoctor } from '../../redux/doctors/doctorsSlice';
-import { fetchAppointments, deleteAppointment } from '../../redux/appointments/appointmentsSlice';
+import { useState } from 'react';
+
 
 export default function DeleteDoctorsContainer() {
+  const [removed, setRemoved] = useState(false);
   const { doctors, isLoading, error } = useSelector((state) => state.doctors);
-  const { appointments } = useSelector((state) => state.appointments);
+ 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-   
-    dispatch(fetchAppointments());
-  },
-  [dispatch]);
+ 
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -21,29 +19,11 @@ export default function DeleteDoctorsContainer() {
   if (error !== undefined) {
     return 'Error... something went wrong';
   }
-
-  const handleDeleteDoctor = async (id) => {
-    try {
-      // Create an array of appointment deletion promises
-      const appointmentDeletionPromises = [];
-
-      // Iterate through the appointments and create promises for each deletion
-      appointments.forEach((appointment) => {
-        if (appointment.doctor_id === id) {
-          appointmentDeletionPromises.push(dispatch(deleteAppointment(appointment.id)));
-        }
-      });
-
-      // Wait for all appointment deletion promises to complete
-      await Promise.all(appointmentDeletionPromises);
-
-      // Once all appointments are deleted, delete the doctor
-      await dispatch(deleteDoctor(id));
-    } catch (error) {
-      throw new Error('Error deleting doctor');
-    }
-  };
-
+ const handleDeleteDoctor = (id) => {
+  dispatch(deleteDoctor(id));
+  setRemoved(true);
+}
+ 
   return (
     <>
       <ul className="doctors-container">
@@ -57,10 +37,11 @@ export default function DeleteDoctorsContainer() {
               {doctor.name}
             </li>
             <button
-              onClick={() => dispatch(handleDeleteDoctor(doctor.id))}
+              onClick={() => handleDeleteDoctor(doctor.id)}
               type="button"
             >
-              Delete Doctor
+              {removed ? 'Removed' : "Delete"}
+             
             </button>
           </ul>
         ))}
