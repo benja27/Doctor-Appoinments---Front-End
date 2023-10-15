3c5ce07fdb6e82ce6f8350386a48deefe2240028
benja27 <50673336+benja27@users.noreply.github.com>
2023-10-15 17:33:22 -0600

    checking styling

--- src/components/index_page/side_menu.jsx
+++ src/components/index_page/side_menu.jsx
@@ -1,14 +1,28 @@
 import Logout from "../authentication/Logout";
+import { useState } from "react"; 
 
 
 function Side_menu() {
+  const [isvisible, setIsVisible] = useState(false);
+
+  const toogleMenu = () => {
+    setIsVisible(!isvisible);
+    alert("hola");
+  }
+
+
   return (
     <article className="d-flex flex-column justify-content-between gap-4 ps-3 mb- px-3 d-none d-md-flex side_menu col-3 col-lg-2">
 
-      <div className="mt-5 ">
+        <div className="text-end mt-3 me-2" >          
+          <i onClick={toogleMenu} className="h3 fas fa-close" ></i>
+        </div>
+
+        <div className="mt-2 ">
+
 
         <div className="text-center">
-          <img src="https://placehold.co/150x50?text=Brand" alt="" />
+          <img className="rounded round" style={{width:"100px"}} src="https://i.pinimg.com/564x/39/97/b8/3997b837ba0548ec1a5430ee31fb43aa.jpg" alt="" />
         </div>
 
         <div className="mt-5 side_menu_cont pt-5 d-flex flex-column gap-4 text-center" style={{}}>
