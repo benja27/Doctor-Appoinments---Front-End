99c5097030dfd61503a46af70a606d36a069edeb
benja27 <50673336+benja27@users.noreply.github.com>
2023-10-15 21:14:39 -0600

    improving side menu

--- src/components/index_page/side_menu.jsx
+++ src/components/index_page/side_menu.jsx
@@ -1,82 +1,92 @@
 import Logout from "../authentication/Logout";
-import { useState } from "react"; 
-
+import { useEffect, useState } from "react";
 
 function Side_menu() {
-  const [isvisible, setIsVisible] = useState(false);
+  const [isvisible, setIsVisible] = useState(true);
 
   const toogleMenu = () => {
-    setIsVisible(!isvisible);
-    alert("hola");
-  }
+    setIsVisible(!isvisible);    
+  };
 
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 768) {
+        setIsVisible(false);
+      }
+    });
+  }, [isvisible]);
 
   return (
-    <article className="d-flex flex-column justify-content-between gap-4 ps-3 mb- px-3 d-none d-md-flex side_menu col-3 col-lg-2">
+    <article className={`d-flex flex-column justify-content-betwee vh-100 ${isvisible ? "side_menu" : "hidden"} ps-3 col-md-3 px-3`}  >
+      <div className="text-end mt-3 me-2">
+        <i onClick={toogleMenu} className={`h3 fas ${isvisible ? "fa-close" : "fa-bars"}`}></i>
+      </div>
 
-        <div className="text-end mt-3 me-2" >          
-          <i onClick={toogleMenu} className="h3 fas fa-close" ></i>
+      <div className={`vh-100 ${isvisible ? "d-flex" : "d-none" } flex-column justify-content-around`}  >
+
+        <div className="">
+          <div className="text-center">
+            <img
+              className="rounded round"
+              style={{ width: "100px" }}
+              src="https://i.pinimg.com/564x/39/97/b8/3997b837ba0548ec1a5430ee31fb43aa.jpg"
+              alt=""
+            />
+          </div>
+
+          <div
+            className="mt-5 side_menu_cont pt-5 d-flex flex-column gap-4 text-center"
+            style={{}}
+          >
+            <section>
+              <a href="/">
+                <h4 className="h4 fw-bold">Home</h4>
+              </a>
+            </section>
+            <section>
+              <a href="/add-doctor">
+                <h4 className="h4 fw-bold">Add Doctor</h4>
+              </a>
+            </section>
+            <section>
+              <a href="/delete-doctor">
+                <h4 className="h4 fw-bold">Delete Doctor</h4>
+              </a>
+            </section>
+            <section>
+              <a href="/appointments">
+                <h4 className="h4 fw-bold">My appnmts</h4>
+              </a>
+            </section>
+            <section>
+              <a href="/add-appointment">
+                <h4 className="h4 fw-bold">Add appnmts</h4>
+              </a>
+            </section>
+
+            <section>
+              <a href="/add-appointment">
+                <h4 className="h4 fw-bold bg-transparent">
+                  {" "}
+                  <Logout></Logout>{" "}
+                </h4>
+              </a>
+            </section>
+          </div>
         </div>
 
-        <div className="mt-2 ">
-
-
-        <div className="text-center">
-          <img className="rounded round" style={{width:"100px"}} src="https://i.pinimg.com/564x/39/97/b8/3997b837ba0548ec1a5430ee31fb43aa.jpg" alt="" />
-        </div>
-
-        <div className="mt-5 side_menu_cont pt-5 d-flex flex-column gap-4 text-center" style={{}}>
-
-          <section>
-            <a href="/">
-              <h4 className="h4 fw-bold">Home</h4>
-            </a>
-          </section>
-          <section>
-            <a href="/add-doctor">
-              <h4 className="h4 fw-bold">Add Doctor</h4>
-            </a>
-          </section>
-          <section>
-            <a href="/delete-doctor">
-              <h4 className="h4 fw-bold">Delete Doctor</h4>
-            </a>
-          </section>
-          <section>
-            <a href="/appointments">
-              <h4 className="h4 fw-bold">My appnmts</h4>
-            </a>
-          </section>
-          <section>
-            <a href="/add-appointment">
-              <h4 className="h4 fw-bold">Add appnmts</h4>
-            </a>
-          </section>
-
-
-          <section>
-            <a href="/add-appointment">
-              <h4 className="h4 fw-bold bg-transparent"> <Logout></Logout> </h4>
-            </a>
-          </section>
-
+        <div className="">
+          <div className="d-flex justify-content-between mb-3 social_media_cont">
+            <i className="fab h5 fa-twitter" />
+            <i className="fab h5 fa-facebook" />
+            <i className="fas h5 fa-envelope" />
+            <i className="fas h5 fa-home" />
+            <i className="fab h5 fa-pinterest" />
+          </div>
+          <h6 className="h6 text-center">@2023 Brand sa de cv</h6>
         </div>
 
       </div>
-
-      <div className="mb-">
-        <div className="d-flex justify-content-between mb-3 social_media_cont">
-          <i className="fab h5 fa-twitter" />
-          <i className="fab h5 fa-facebook" />
-          <i className="fas h5 fa-envelope" />
-          <i className="fas h5 fa-home" />
-          <i className="fab h5 fa-pinterest" />
-        </div>
-        <h6 className="h6 text-center">
-          @2023 Brand sa de cv
-        </h6>
-      </div>
-
     </article>
   );
 }
