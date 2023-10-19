73f2c3bea661112af83b7121b94653c881f3edf9
emarkees <rphillipse@gmail.com>
2023-10-19 08:25:15 -0600

    Refactor the App component to ES6 arrow function as requested by the PR

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import DoctorForm from './components/doctors/DoctorForm';
 import DeleteDoctorContainer from './components/doctors/DeleteDoctorContainer';
 import AppointmentsContainer from './components/appointments/AppointmentsContainer';
-// import AppointmentForm from './components/appointments/AppointmentForm';
-// import FullAppointmentForm from './components/appointments/FullAppointmentForm';
 import SplashScreen from './components/splashScreen/SplashScreen';
 import Login from './components/authentication/Login';
-// import Logout from './components/authentication/Logout';
 import Signup from './components/authentication/Signup';
 import BookForm from './components/appointments/AppointmentForm';
-
 import MainPage from './components/MainPage';
 import SetAppoiment from './components/SetAppoiment';
 import Show from './components/Show';
-
 import './App.css';
 import { authSuccess } from './redux/currentUser/currentUserSlice';
 import { fetchDoctors } from './redux/doctors/doctorsSlice';
 
-function App() {
+const App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchDoctors());
@@ -37,32 +32,21 @@ function App() {
   [dispatch]);
 
   const { isAuth, currentUser } = useSelector((state) => state.currentUser);
-  // console.log(currentUser);
-  // console.log(isAuth);
 
   if (isAuth) {
     return (
       <>
-
         <Routes>
-
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-
           <Route path="/app" element={<BookForm />} />
-
           <Route path="/" element={<MainPage user={currentUser} />} />
           <Route path="/add-doctor" element={<DoctorForm />} />
-
           <Route path="/add-appointment" element={<SetAppoiment />} />
-
           <Route path="/set_appoinment" element={<BookForm />} />
-
           <Route path="doctors/:doctorId" element={<Show />} />
           <Route path="/delete-doctor" element={<DeleteDoctorContainer />} />
           <Route path="/appointments" element={<AppointmentsContainer />} />
-          {/* <Route path="/add-appointment/:doctorId" element={<AppointmentForm />} /> */}
-
         </Routes>
       </>
     );
